74b36490b599bdd392ab5c5e2bcb4b59eb823c00
Yonlero <abraaobrg150@gmail.com>
2022-09-01 16:57:22 -0300

    Initial Commit

---
+++ src/test/java/com/rp/interview_rp/InterviewRpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rp.interview_rp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewRpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
