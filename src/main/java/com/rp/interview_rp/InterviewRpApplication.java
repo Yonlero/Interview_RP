5c88590a38910db4791842cf03a8f0c4445d4307
Yonlero <abraaobrg150@gmail.com>
2022-09-01 18:47:18 -0300

    Base Entities | Health test

--- src/main/java/com/rp/interview_rp/InterviewRpApplication.java
+++ src/main/java/com/rp/interview_rp/InterviewRpApplication.java
@@ -1,13 +1,25 @@
 package com.rp.interview_rp;
 
+import com.rp.interview_rp.model.services.ClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterviewRpApplication {
+public class InterviewRpApplication implements CommandLineRunner {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    ClientService service;
 
     public static void main(String[] args) {
         SpringApplication.run(InterviewRpApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        service.testDB().forEach(x -> logger.info(x.toString()));
+    }
 }
